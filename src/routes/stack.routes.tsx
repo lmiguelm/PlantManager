b5461e3341781deb8e3553069787542969e8268c
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2021-05-31 15:56:20 -0300

    Inibindo tela de cadastro caso usuário já esteja cadastrado

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import colors from '../styles/colors';
@@ -9,23 +9,41 @@ import { Confirmation } from '../pages/Confirmation';
 import { PlantSave } from '../pages/PlantSave';
 
 import { AuthRoutes } from './tab.routes';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const { Screen, Navigator } = createStackNavigator();
 
-export const StackRoutes: React.FC = () => (
-  <Navigator
-    headerMode="none"
-    screenOptions={{
-      cardStyle: {
-        backgroundColor: colors.white,
-      },
-    }}
-  >
-    <Screen name="Welcome" component={Welcome} />
-    <Screen name="UserIdentification" component={UserIdentification} />
-    <Screen name="Confirmation" component={Confirmation} />
-    <Screen name="PlantSelect" component={AuthRoutes} />
-    <Screen name="PlantSave" component={PlantSave} />
-    <Screen name="MyPlants" component={AuthRoutes} />
-  </Navigator>
-);
+export const StackRoutes: React.FC = () => {
+  const [isLogged, setIsLogged] = useState(false);
+
+  useEffect(() => {
+    async function getUser() {
+      const user = await AsyncStorage.getItem('@plantmanager:user');
+      setIsLogged(!!user);
+    }
+    getUser();
+  }, []);
+
+  return (
+    <Navigator
+      headerMode="none"
+      screenOptions={{
+        cardStyle: {
+          backgroundColor: colors.white,
+        },
+      }}
+    >
+      {!isLogged && (
+        <>
+          <Screen name="Welcome" component={Welcome} />
+          <Screen name="UserIdentification" component={UserIdentification} />
+          <Screen name="Confirmation" component={Confirmation} />
+        </>
+      )}
+
+      <Screen name="PlantSelect" component={AuthRoutes} />
+      <Screen name="PlantSave" component={PlantSave} />
+      <Screen name="MyPlants" component={AuthRoutes} />
+    </Navigator>
+  );
+};
